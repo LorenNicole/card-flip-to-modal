7ea5e5b2c9600ebe93768190ce8076bab37b8f7c
Loren Nicole Simons <LorenNicole@users.noreply.github.com>
2026-07-16 18:34:23 -0400

    Scaffold Card Flip to Modal block

---
+++ build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'build' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'fun-gutenberg-blocks/card-flip-to-modal',
+		'version' => '0.1.0',
+		'title' => 'Card Flip to Modal',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'A Gutenberg block that opens custom card content in a modal.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'card-flip-to-modal',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c8469b2b9a11aed5a22c');
