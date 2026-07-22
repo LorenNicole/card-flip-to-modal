73572ad0ee400aa67e8ca342f87185872c73c57f
Loren Nicole Simons <LorenNicole@users.noreply.github.com>
2026-07-22 13:33:15 -0400

    Convert modal constants to TypeScript more files

--- build/card-flip-to-modal/index.asset.php
+++ build/card-flip-to-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '03d093d5d38caca89d58');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5334541a8731ce7d026e');
