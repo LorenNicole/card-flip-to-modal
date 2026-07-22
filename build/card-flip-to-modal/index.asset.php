11043f7325d012ecc1fd2f2a524bf9ef4c8b21f0
Loren Nicole Simons <LorenNicole@users.noreply.github.com>
2026-07-22 14:06:50 -0400

    Convert parent block registration to TypeScript

--- build/card-flip-to-modal/index.asset.php
+++ build/card-flip-to-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'daa45675bcda34756f07');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9d73b654c54c660fd9f1');
